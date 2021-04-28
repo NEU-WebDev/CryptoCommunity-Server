c193e166798099f05c58740747cf1b513076c46a
richardcastaneda <61243707+richardcastaneda@users.noreply.github.com>
2021-04-28 10:44:45 -0700

    Heroku server change

--- src/main/java/com/example/CryptoCommunityServer/repositories/AdminUserRepository.java
+++ src/main/java/com/example/CryptoCommunityServer/repositories/AdminUserRepository.java
@@ -1,8 +1,14 @@
 package com.example.CryptoCommunityServer.repositories;
 
 import com.example.CryptoCommunityServer.models.AdminUserJoined;
+import com.example.CryptoCommunityServer.models.Post;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface AdminUserRepository extends CrudRepository<AdminUserJoined, Long> {
+
+  @Query(value="SELECT * FROM joined_admin_user WHERE id=:userId", nativeQuery = true)
+  public Post findPostById(@Param("userId") String userId);
 
 }
